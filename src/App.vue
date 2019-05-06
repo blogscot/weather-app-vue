39d6dc712ed4cca27f2a801b86c1ded87342b5e5
Iain Diamond <iain.diamond@gmail.com>
2019-05-06 13:06:12 +0100

    Transfer app from old version

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Weather minmax :main="weather.main" :dt="weather.dt" :sys="weather.sys"></Weather>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Weather from "./components/Weather";
 
 export default {
-  name: "app",
+  name: "App",
   components: {
-    HelloWorld
+    Weather
+  },
+  async created() {
+    console.log(process.env);
+    this.weather = await this.asyncFetchWeatherData();
+  },
+  methods: {
+    async asyncFetchWeatherData() {
+      return new Promise(resolve => {
+        setTimeout(() => resolve(this.fetchWeatherData()), 200);
+      });
+    },
+    fetchWeatherData() {
+      return {
+        coord: { lon: 139, lat: 35 },
+        sys: { country: "JP", sunrise: 1369769524, sunset: 1369821049 },
+        weather: [
+          {
+            id: 804,
+            main: "clouds",
+            description: "overcast clouds",
+            icon: "04n"
+          }
+        ],
+        main: {
+          temp: 289.5,
+          humidity: 89,
+          pressure: 1013,
+          temp_min: 287.04,
+          temp_max: 292.04
+        },
+        wind: { speed: 7.31, deg: 187.002 },
+        rain: { "3h": 0 },
+        clouds: { all: 92 },
+        dt: 1369824698,
+        id: 1851632,
+        name: "Shuzenji",
+        cod: 200
+      };
+    }
+  },
+  data() {
+    return {
+      weather: {}
+    };
   }
 };
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Weather.vue
@@ -0,0 +1,60 @@
+<template>
+  <section v-if="main">
+    <div>Current Temperature: {{ fromKelvin(main.temp) }}</div>
+    <div v-if="minmax || full">
+      <div>Today's Min: {{ fromKelvin(main.temp_min) }}</div>
+      <div>Today's Max: {{ fromKelvin(main.temp_max) }}</div>
+    </div>
+    <div v-if="full">
+      <div>Humidity: {{ main.humidity }}</div>
+      <div>Pressure: {{ main.pressure }}</div>
+      <div>Sunrise: {{ fromUnixTime(sys.sunrise) }}</div>
+      <div>Sunset: {{ fromUnixTime(sys.sunset) }}</div>
+      <div>Taken at: {{ fromUnixTime(dt) }}</div>
+    </div>
+  </section>
+  <section v-else>
+    <div>Loading...</div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Weather",
+  props: {
+    minmax: {
+      type: Boolean,
+      default: false
+    },
+    full: {
+      type: Boolean,
+      default: false
+    },
+    main: {
+      temp: Number,
+      humidity: Number,
+      pressure: Number,
+      temp_min: Number,
+      temp_max: Number
+    },
+    dt: Number,
+    sys: {
+      sunrise: Number,
+      sunset: Number
+    }
+  },
+  methods: {
+    fromKelvin(temp) {
+      return (temp - 273.15).toFixed(2);
+    },
+    fromUnixTime(unixtime) {
+      // let locale = navigator.languages[0];
+      let locale = "en-US";
+      let localtime = new Date(unixtime * 1000).toLocaleString(locale, {
+        timeZoneName: "short"
+      });
+      return localtime.slice(localtime.indexOf(" ") + 1);
+    }
+  }
+};
+</script>
